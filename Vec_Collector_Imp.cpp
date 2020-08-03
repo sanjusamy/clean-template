9796f803d44899522d61821a4ebeb13953fd84bc
Karuppanasamy A.S <48689694+sanjusamy@users.noreply.github.com>
2020-08-03 16:10:56 +0530

    implementation of vehicle collector

--- Vec_Collector_Imp.cpp
+++ Vec_Collector_Imp.cpp
@@ -1,1 +1,36 @@
 
+#include 'Vec_Collector.h"
+
+Vehicle_StorageMsgConfig Vehicle_Info[MAX_VEHICLE];
+
+update_veh_param(unsigned short index,  const Vehicle_InputMsgConfig *inputMsg)   
+{
+  if(inputMsg.paramId == MOTOR_TEMP)
+  {
+    Vehicle_Info[index].motorTempValue = inputMsg.paramValue;
+  }
+  else if (inputMsg.paramId == BATTERY_TEMP)
+  {
+    Vehicle_Info[index].batteryTempValue = inputMsg.paramValue;
+  }
+  else if ((inputMsg.paramId == BATTERY_SOC))
+  {
+    Vehicle_Info[index].batterySoc = inputMsg.paramValue;
+  }
+  else
+  {
+  }
+}
+
+void update_vehicle_information(const Vehicle_InputMsgConfig *inputMsg, Vehicle_StorageMsgConfig *storage)
+{
+  for(unsigned short index=0; index < MAX_VEHICLE; index++)
+  {
+    if(inputMsg.vehicleId == Vehicle_Info[index].vehicleId)
+    {
+      update_veh_param(index,  inputMsg);
+      break;            
+    }
+  }  
+  return (Vehicle_Info[index]);
+}
