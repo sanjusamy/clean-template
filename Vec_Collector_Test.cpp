a53f068a38efb75171b908525c1ff070a30eca17
Karuppanasamy A.S <48689694+sanjusamy@users.noreply.github.com>
2020-08-03 14:17:47 +0530

    add test case to validate vechile update param

--- Vec_Collector_Test.cpp
+++ Vec_Collector_Test.cpp
@@ -1,7 +1,35 @@
 #include <gtest/gtest.h>
- 
-TEST(VitalsTest, ) {
-  
+
+typedef enum
+{
+ MOTOR_TEMP = 0,
+ BATTERY_TEMP,
+ BATTERY_SOC 
+}Vehicle_Param;
+
+typedef VehicleId_type unsigned short;
+
+typedef struct{
+  VehicleId_type vehicleId;
+  Vehicle_Param paramId;
+  float paramValue; 
+}Vehicle_InputMsgConfig;
+
+typedef struct{
+  VehicleId_type vehicleId;
+  float motorTempValue; 
+  float batteryTempValue;
+  float batterySoc;
+}Vehicle_StorageMsgConfig;
+
+
+
+
+TEST(VitalsTest, when_a_parameter_received_from_a_vehicle_should_update_aginst_vechile_id) 
+{ 
+ Vehicle_InputMsgConfig inputMsg = {1, MOTOR_TEMP, 23.5};
+ Update_Vehicle_param(&inputMsg, &vehicleStorage);
+ EXPECT_NEAR(23.5, vehicleStorage.motorTempValue, 0.001);  
 }
 
  
