67576d8b624469f31f4a67ddddf4fe5303651a51
Karuppanasamy A.S <48689694+sanjusamy@users.noreply.github.com>
2020-08-03 14:26:03 +0530

    Common strcutures for vehicle collector

--- Vec_Collector.h
+++ Vec_Collector.h
@@ -1,1 +1,22 @@
 
+typedef enum
+{
+ MOTOR_TEMP = 0,
+ BATTERY_TEMP,
+ BATTERY_SOC 
+}Vehicle_Param;
+
+typedef VehicleId_type unsigned short;
+
+typedef struct{
+  VehicleId_type vehicleId;
+  Vehicle_Param paramId;
+  float paramValue; 
+}Vehicle_InputMsgConfig;
+
+typedef struct{
+  VehicleId_type vehicleId;
+  float motorTempValue; 
+  float batteryTempValue;
+  float batterySoc;
+}Vehicle_StorageMsgConfig;
