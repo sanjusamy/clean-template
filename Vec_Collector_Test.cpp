b33b25cd4882064b5a89e70c5b09c524be74abd3
Karuppanasamy A.S <48689694+sanjusamy@users.noreply.github.com>
2020-08-04 16:41:03 +0530

    added test for complete coverage

--- Vec_Collector_Test.cpp
+++ Vec_Collector_Test.cpp
@@ -4,10 +4,19 @@
 
 TEST(VehicleCollectorTest, when_a_parameter_received_from_a_vehicle_should_update_aginst_vechile_id) 
 { 
+ 
  Vehicle_InputMsgConfig inputMsg = {1, MOTOR_TEMP, 23.5};
  Vehicle_StorageMsgConfig vehicleRecord;
  update_vehicle_information(&inputMsg, &vehicleRecord);
  EXPECT_NEAR(23.5, vehicleRecord.motorTempValue, 0.01);  
+ 
+ inputMsg = {1, BATTERY_TEMP, 22.0};
+ update_vehicle_information(&inputMsg, &vehicleRecord);
+ EXPECT_NEAR(22.0, vehicleRecord.batteryTempValue, 0.01); 
+   
+ inputMsg = {1, BATTERY_SOC, 75.0};
+ update_vehicle_information(&inputMsg, &vehicleRecord);
+ EXPECT_NEAR(75.0, vehicleRecord.batterySocValue, 0.01);  
 }
 
  
